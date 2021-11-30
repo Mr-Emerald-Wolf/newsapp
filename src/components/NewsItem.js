9b5b68cb8836590007d24bf2f092dd8ebe18ad2b
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2021-11-30 17:44:52 +0530

    NewsItem Bugs fixed

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,8 +5,8 @@ export class NewsItem extends Component {
 
     render() {
         let imageUrl = this.props.articleData["media"];
-        let title = (this.props.articleData["title"]==null ? " ": this.props.articleData["summary"]).slice(0,60);
-        let description = (this.props.articleData["summary"]==null ? " ": this.props.articleData["summary"]).slice(0,120);
+        let title = (this.props.articleData["title"]==null ? " ": this.props.articleData["title"]);
+        let description = (this.props.articleData["summary"]==null ? " ": this.props.articleData["summary"]);
         let newsUrl = this.props.articleData["link"];
         let author = this.props.articleData["author"];
         let time = this.props.articleData["published_date"];
@@ -17,12 +17,12 @@ export class NewsItem extends Component {
                     <div className="card bg-custom-1">
                         <img src={imageUrl} className="card-img-top" alt="News Item" />
                         <div className="card-body">
-                            <h5 className="card-title text-pink">{title}...</h5>
-                            <p className="card-text text-light">{description===null ? null : description+"..."}</p>
+                            <h5 className="card-title text-pink">{title.slice(0,60)+"..."}</h5>
+                            <p className="card-text text-light">{description.slice(0,120)+"..."}</p>
                             <a href={newsUrl} className="btn btn-dark fw-bold">Read More</a>
                         </div>
                         <div className="card-footer">
-                            <p className="text-gray">By {author===null ? "NewsNetwork" : author}</p>
+                            <p className="text-gray">By {author=== null ? "NewsNetwork" : author}</p>
                             <small className="text-gray">Published on {time}</small>
                         </div>
                     </div>
