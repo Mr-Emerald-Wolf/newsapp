feb51c71d21fca4079511b0a730f491b0e55d651
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2021-11-30 17:37:01 +0530

    Emergency Fix

--- src/components/News.js
+++ src/components/News.js
@@ -25,71 +25,85 @@ export class News extends Component {
         super();
         this.state = {
             articles: [],
-            loading: true, 
+            loading: true,
             currentPage: 1,
             totalPages: 1,
             totalResults: 50
-     }
+        }
     }
 
     capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
     fetchData = async () => {
-            let url = `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=${this.props.category}&page_size=9&page=${this.state.currentPage+1}`
-            const req = { headers: { 'x-api-key': 'll0DexesayjJrMbd6BBiRVQb70kXHARxgSGpNwFXgIo' } };
-    
-            let data = await fetch(url,req);
-            let parsedData = await data.json();
-           
-            this.setState({
-                currentPage: this.state.currentPage + 1,
-                data: parsedData.articles,
-                articles: this.state.articles.concat(parsedData.articles),
-                loading: false
-            });
-            
-        }
-    fetchMore = () => {
+        let url = `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=${this.props.category}&page_size=9&page=${this.state.currentPage + 1}`
+        const req = { headers: { 'x-api-key': 'll0DexesayjJrMbd6BBiRVQb70kXHARxgSGpNwFXgIo' } };
+
+        let data = await fetch(url, req);
+        let parsedData = await data.json();
+
+        this.setState({
+            currentPage: this.state.currentPage + 1,
+            data: this.removeDuplicates(parsedData.articles),
+            articles: this.state.articles.concat(parsedData.articles),
+            loading: false
+        });
         
-        setTimeout(() => {this.fetchData()}, 1000);
+
+    }
+    fetchMore = () => {
+
+        setTimeout(() => { this.fetchData() }, 1000);
         //console.log("Hit1");
         //console.log(this.state.articles.length);
-        
+
     }
-   
+
 
     async componentDidMount() { //API Fetch here
         const reqOptions = { headers: { 'x-api-key': 'll0DexesayjJrMbd6BBiRVQb70kXHARxgSGpNwFXgIo' } };
         let url = `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&page_size=9&page=1&topic=${this.props.category}`
-        let data = await fetch(url,reqOptions);
+        let data = await fetch(url, reqOptions);
         let parsedData = await data.json();
         let pagesAvailable = Math.ceil((parsedData.total_hits) / this.props.pageSize);
 
         this.setState({
             data: parsedData.articles,
-            articles: parsedData.articles,
+            articles: this.removeDuplicates(parsedData.articles),
             totalPages: pagesAvailable,
             //totalResults: parsedData.total_hits,
             loading: false
 
         });
-
-
+        
 
         // //console.log(this.state.currentPage);
         // console.log(totalPages);
         //console.log(url);
         //console.log(data);
         //console.log(parsedData.articles);
-        console.log(parsedData.totalResults);
+        //console.log(parsedData.totalResults);
         // //console.log("CDM");
         // console.log(this.state.data.slice(0,6));
         // console.log(this.state.data.slice({this.props.pageSize},18));
 
     }
 
+    removeDuplicates = (duplicates) => {
+        const flag = {};
+        const unique = [];
+        duplicates.forEach(elem => {
+            if (!flag[elem.title]) {
+                flag[elem.title] = true;
+                unique.push(elem);
+            }
+        });
+        //console.log(flag);
+        
+        return unique;
 
+
+    }
 
     render() {
         return (
@@ -99,7 +113,7 @@ export class News extends Component {
                 <InfiniteScroll
                     dataLength={this.state.articles.length} //This is important field to render the next data
                     next={this.fetchMore}
-                    hasMore={this.state.articles.length<=this.state.totalResults}
+                    hasMore={this.state.articles.length <= this.state.totalResults}
                     loader={<Spinner />}
                     endMessage={
                         <div className="container d-flex my-3 justify-content-center">
