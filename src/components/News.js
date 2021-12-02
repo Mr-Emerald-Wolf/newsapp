0323db967e7242764bfb33afeac94023c4ebb8bf
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2021-12-02 13:10:25 +0530

    CHecking errors

--- src/components/News.js
+++ src/components/News.js
@@ -37,7 +37,8 @@ export class News extends Component {
     }
     fetchData = async () => {
         let url = `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&lang=en&topic=${this.props.category}&page_size=9&page=${this.state.currentPage + 1}`
-        const req = { headers: { 'x-api-key': '$api_key' } };
+        const apiKEY = process.env.api_key;
+        const req = { headers: { 'x-api-key': apiKEY } };
 
         let data = await fetch(url, req);
         let parsedData = await data.json();
@@ -61,9 +62,10 @@ export class News extends Component {
 
 
     async componentDidMount() { //API Fetch here
-        const reqOptions = { headers: { 'x-api-key': '$api_key' } };
+        const apiKEY = process.env.api_key;
+        const req = { headers: { 'x-api-key': apiKEY } };
         let url = `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&lang=en&page_size=9&page=1&topic=${this.props.category}`
-        let data = await fetch(url, reqOptions);
+        let data = await fetch(url, req);
         let parsedData = await data.json();
         let pagesAvailable = Math.ceil((parsedData.total_hits) / this.props.pageSize);
         let uniqueData = this.removeDuplicates(parsedData.articles)
