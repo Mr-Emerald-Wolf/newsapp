5a71b66837058323242202e78fe0b10032022be9
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-01-24 22:58:40 +0530

    V1 is finished.

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,17 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
         return (
-            <div>
-                <div className="container m-2 d-flex justify-content-center  gap-2">
-                    <h1 className="display-1 text-light title-1 ">About</h1>
+            <>
+                <div className="container">
+                    <h1 className="display-2 text-light ">About</h1>
+                    <p className="display-6 fw-light text-light">A place to relax and read the latest news late at night. </p>
+                    <p className=" text-light ">Made By Shivam Sharma </p>
+                    <p className=" text-light ">Using React, Bootstrap, react-Infinite-scroll-component and React Router v6</p>
+                    <p className=" text-light ">{"</newscatcher> Free Api to fetch news."}</p>
+                    <a href="https://youtube.com/playlist?list=PLu0W_9lII9agx66oZnT6IyhcMIbUMNMdt" className=" text-light ">Made using guidance from Learn ReactJS Playlist by CodeWithHarry.</a>
+
                 </div>
-            </div>
+            </>
         )
     }
 }
